36730cc4a83c30bb5108fe26c3ae140568ab8129
randomer108 <jacob.gibbs@gmx.co.uk>
2023-12-12 12:45:02 +0000

    Thought day 7 was done but something is wrong.

--- day7.py
+++ day7.py
@@ -4,15 +4,17 @@ import math as m
 
 data = open("./AOC23/data/day7.txt").readlines()
 data=list(filter(None,[re.sub('\\n','',d) for d in data]))
-Hands = [d[:5] for d in data]
-Bids = [d[6:9] for d in data]
+
 
 
 class hand:
-    def __init__(self,inp):
+    def __init__(self,inp,score):
         self.hand_raw=inp
         self.hand_counted={}
         self.type=str()
+        self.score= int(score)
+
+
     def count_cards(self):
         cards = {}
         for card in enumerate(self.hand_raw):
@@ -29,21 +31,34 @@ class hand:
         elif max(cards.values()) ==2 and len(cards)==3: self.type='2pair'
         elif max(cards.values()) ==2 and len(cards)==4: self.type='1pair'
         elif max(cards.values()) ==1:                  self.type='highCard'
-        else: pass #raise Exception('Hand type not recognised')
+        else: raise Exception('Hand type not recognised')
         return self
 
-        
-        
-    
-""" x={'a':100,'b':20}
-print(max(x.values()))
-print(len(x)) """ 
 
-for H in Hands:
-    H= hand(H)
-    H = H.count_cards()
-    print(H.hand_counted)
-    print(len(H.hand_counted))
-    print(max(H.hand_counted.values()))
-    print(H.type)
+# sorting
+def custom_sort(instance):
+    order_dict = {'A': 0, 'K': 1, 'Q': 2, 'J': 3, 'T': 4, '9': 5, '8': 6, '7': 7, '6': 8, '5': 9, '4': 10, '3': 11, '2': 12}
+    return [order_dict[char] for char in instance.hand_raw]
 
+   
+# Generate Hands + set type based on cards in hand.
+Hands=[hand(inp=d[:5], score=d[6:9]).count_cards() for d in data] # data are strings with 5 characters, space, then the number (max 3 digits)
+
+# Sort the groups, syartinig with highest scoring, and working down!
+Hands_sorted = []
+for handtype in ['5ofaK', '4ofaK', 'fullHouse', '3ofaK', '2pair', '1pair', 'highCard']:
+    sorted_instances = sorted([H for H in Hands if H.type==handtype], key=custom_sort)
+    Hands_sorted.append(sorted_instances)
+
+Hands_sorted=[H for HH in Hands_sorted for H in HH] # flatten
+
+
+# Calculate answer
+Answer=0
+i=len(Hands) # ranks are 1 for weakest hand...
+for hand in Hands_sorted:
+    print(i)
+    Answer +=i*hand.score
+    i-=1
+
+print(Answer)
