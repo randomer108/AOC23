cbdb76997c7a6321979f2d8d067f0d197bfa02d0
randomer108 <jacob.gibbs@gmx.co.uk>
2023-12-13 11:24:51 +0000

    Day 7 done! woop - really fun

--- day7.py
+++ day7.py
@@ -11,7 +11,11 @@ class hand:
     def __init__(self,inp,score):
         self.hand_raw=inp
         self.hand_counted={}
+        self.hand_counted2={}
+
         self.type=str()
+        self.type2=str()
+
         self.score= int(score)
 
 
@@ -33,6 +37,48 @@ class hand:
         elif max(cards.values()) ==1:                  self.type='highCard'
         else: raise Exception('Hand type not recognised')
         return self
+    
+    def count_cards_2(self):
+        # Expects card to already be counted by count_cards()
+
+        try:
+            wild = self.hand_counted['J']
+        except:
+            wild=0
+        
+        self.hand_counted2 = {card:value for card,value in self.hand_counted.items() if card != 'J'}
+        #print(f'{self.hand_counted2} and {wild}')
+        
+        cards = self.hand_counted2 # bt more terse
+
+        if   self.hand_raw =='JJJJJ':                        self.type2='5ofaK' 
+        elif max(cards.values()) ==5:                        self.type2='5ofaK'
+        elif max(cards.values())+wild==5:                    self.type2='5ofaK'
+ 
+        elif max(cards.values()) ==4:                        self.type2='4ofaK'
+        elif max(cards.values())+wild ==4:                   self.type2='4ofaK'
+
+        elif max(cards.values()) ==3 and len(cards) ==2:     self.type2='fullHouse'
+        elif max(cards.values())+wild ==3 and len(cards) ==2:self.type2='fullHouse'
+
+        elif max(cards.values()) ==3 and len(cards)==3:      self.type2='3ofaK'
+        elif max(cards.values())+wild ==3 and len(cards)==3: self.type2='3ofaK'
+
+        elif max(cards.values()) ==2 and len(cards)==3:      self.type2='2pair' # with 1 J  - 2 pair is dominated by 3ofaK. With >2 J, 2 pair is obviously also dominated.
+
+        elif max(cards.values()) ==2 and len(cards)==4:      self.type2='1pair'
+        elif max(cards.values())+wild ==2 and len(cards)==4: self.type2='1pair'  
+
+        elif max(cards.values()) ==1   and wild==0:          self.type2='highCard' # added condition of no wilds. If you have wilds, you should not get here! So this is wrong!
+        else: raise Exception('Hand type not recognised')
+
+        self.hand_counted2 = cards # undo the terse thing
+        return self
+
+#  1  # 
+
+# Generate Hands + set type based on cards in hand.
+Hands=[hand(inp=d[:5], score=d[6:]).count_cards() for d in data] # data are strings with 5 characters, space, then the number (max 3 digits)
 
 
 # sorting
@@ -40,25 +86,47 @@ def custom_sort(instance):
     order_dict = {'A': 0, 'K': 1, 'Q': 2, 'J': 3, 'T': 4, '9': 5, '8': 6, '7': 7, '6': 8, '5': 9, '4': 10, '3': 11, '2': 12}
     return [order_dict[char] for char in instance.hand_raw]
 
-   
-# Generate Hands + set type based on cards in hand.
-Hands=[hand(inp=d[:5], score=d[6:9]).count_cards() for d in data] # data are strings with 5 characters, space, then the number (max 3 digits)
-
-# Sort the groups, syartinig with highest scoring, and working down!
+# Sort the groups, startinig with highest scoring, and working down!
 Hands_sorted = []
 for handtype in ['5ofaK', '4ofaK', 'fullHouse', '3ofaK', '2pair', '1pair', 'highCard']:
     sorted_instances = sorted([H for H in Hands if H.type==handtype], key=custom_sort)
     Hands_sorted.append(sorted_instances)
-
 Hands_sorted=[H for HH in Hands_sorted for H in HH] # flatten
-
 
 # Calculate answer
 Answer=0
 i=len(Hands) # ranks are 1 for weakest hand...
 for hand in Hands_sorted:
-    print(i)
     Answer +=i*hand.score
     i-=1
-
 print(Answer)
+
+
+# 2 #
+# Now jacks are low, but wild!
+# So change soring order
+# And a new processing function that take out the Jacks, and adds them to the 'max' operator?
+
+# Recount the same hands
+Hands=[hand.count_cards_2() for hand in Hands]
+
+
+# new sorting (Jacks at end)
+def custom_sort_2(instance):
+    order_dict = order_dict = {'A': 0, 'K': 1, 'Q': 2, 'T': 3, '9': 4, '8': 5, '7': 6, '6': 7, '5': 8, '4': 9, '3': 10, '2': 11, 'J': 12}
+    return [order_dict[char] for char in instance.hand_raw]
+
+# Sort the groups, startinig with highest scoring, and working down!
+Hands_sorted2 = []
+for handtype in ['5ofaK', '4ofaK', 'fullHouse', '3ofaK', '2pair', '1pair', 'highCard']:
+    sorted_instances = sorted([H for H in Hands if H.type2==handtype], key=custom_sort_2)
+    Hands_sorted2.append(sorted_instances)
+Hands_sorted2=[H for HH in Hands_sorted2 for H in HH] # flatten
+
+# Calculate answer
+Answer2=0
+i=len(Hands) # ranks are 1 for weakest hand...
+for hand in Hands_sorted2:
+    Answer2 +=i*hand.score
+    i-=1
+print(Answer2)
