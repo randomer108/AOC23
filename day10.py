323966e9bcfe421056d3fcf8dfa225b8ba36da98
randomer108 <jacob.gibbs@gmx.co.uk>
2023-12-18 18:19:07 +0000

    Give up day 10 part 2. Too many corner cases, not having fun

--- day10.py
+++ day10.py
@@ -6,8 +6,7 @@ import timeit
 data = open("./AOC23/data/day10.txt").readlines()
 data = np.array([list(line.strip()) for line in data])      
 
-Start = np.where(data=='S')
-print(Start[0],Start[1])
+
 class pointer:
     def __init__(self,y,x,symbol,direction):
         self.x=x
@@ -68,31 +67,82 @@ class pointer:
             self.y-=1
         elif self.current_direction=='D':
             self.y+=1   
- 
+
+
+# Take start and initialise pointers
+Start = np.where(data=='S')
+
 pointer1 = pointer(16,38,'-','R')
 pointer1.move()
-#print(' I am: '+pointer1.next_symbol+'  heading: '+pointer1.current_direction)
 
 pointer2 = pointer(16,36,'F','D')
 pointer2.move()
-#print(' I am: '+pointer2.next_symbol+'  heading: '+pointer2.current_direction)
+
+pipe=[(Start[0][0],Start[1][0])] # part 2
 
 print(data)
 i=1
 while abs(pointer1.x-pointer2.x) + abs(pointer1.y-pointer2.y)>1:
     pointer1.read()
-    #print(' I am: '+pointer1.next_symbol+'  heading: '+pointer1.current_direction)
     pointer1.move()
-    
+    pipe.append((pointer1.x,pointer1.y))
+
     pointer2.read()
-    #print(' I am: '+pointer2.next_symbol+'  heading: '+pointer2.current_direction)
     pointer2.move()
+    pipe.append((pointer2.x,pointer2.y))
     i+=1
     if i>10000: break
     
 print("done " + str(i+1))
 
+# 2 #
+# What's inside? 
+# Well drawing a ray from any point to the outer edge - it must cross [0,2,4,6,8...] if it's OUTSIDE, and [1,3,5,7,9,...] if it's inside.
+# So log the path points
+# So draw the ray, count intersections with the path
+
+pipe=set(pipe)
+insides=0
 
 
+point = set((46,53))
+
+for X in range(len(data)):
+    for Y in range(len(data[0])):
+        point = (X,Y)
+        if point in pipe:
+            continue
+        
+        SYMBOL = data[X,Y] # need this
+
+        # Maye a ray to the side
+        rayE=[(x,point[1]) for x in range(point[0])]
+       
+        # See how many times it crosses the pipe
+
+        crash=False # currenty in the pipe?
+        crosses=0 # number of changes of <pipe to ¬pipe> or vice versa.
+
+        for point in rayE:
+            if point in pipe and SYMBOL=='-':
+                    continue
+            if point in pipe and SYMBOL!='-':
+                    if crash == False:
+                        crash=True
+                        crosses+=1
+                    if crash==True:
+                        continue
+
+            if point not in pipe:
+                if crash==True:
+                    crash=False
+                    crosses+=1
+                if crash==False:
+                    continue
+
+        if crosses %2!=0:
+            insides+=1
+
+print(insides)
 
 
