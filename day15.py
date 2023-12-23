b7e93e4c0f7778d478b529488321019eb8e3c835
randomer108 <jacob.gibbs@gmx.co.uk>
2023-12-23 12:22:02 +0000

    Done day 15! Noice. Bit messy, as I got a bit last - dictionaries still confuse me a bit. Oh well works!

--- day15.py
+++ day15.py
@@ -7,11 +7,22 @@ data = open("./data/day15.txt")
 data=[d.split(',') for d in data]
 data=[d for dd in data for d in dd]
 
+class lens:
+    def __init__(self,label,value):
+        self.lens_stuff ={label:value}
+
+
 class instruction:
     def __init__(self,string):
         self.hash=int()
         self.string_raw=string
-        self.label=re.sub('=.*|-','',string)
+        
+        self.box=int()
+
+        if re.search('=',self.string_raw):
+            self.label=re.sub('=.*','',string)
+        else:
+            self.label=re.sub('-','',string)
 
         match = re.search(r'\d+', string)
         if match:
@@ -29,35 +40,61 @@ class instruction:
         return self
 
     
-    def process_instruction(self):
+    def process_instruction(self,lens_array):
 
-        box=lens_array[self.hash]
+        # get box
+        x=0
+        for i in self.label:
+            x+=ord(i)
+            x=x*17
+            x=x%256
+        self.box=int(x)
 
+        box=lens_array[self.box]
+        
+        # process the instruction
         if re.search('-',self.string_raw):
-            if self.label in box:
-                box.remove(self.label)
-                lens_array[self.hash]= box
-
+            for dicts in box:
+                if self.label in dicts:
+                    box.remove(dicts)
+                
         if re.search('=',self.string_raw):
-            lens=lens(self.label,self.strength)
-
-
-
-
-
-class lens:
-    def __init__(self,label,value):
-        self.properties ={label:value}
-
-
+            self.lens=lens(self.label,self.strength)
+            found_lens=False
+            if len(box)>0:
+                count=0
+                for dicts in box:
+                    if self.label in dicts:
+                        box[count]=self.lens.lens_stuff
+                        found_lens=True
+                    count+=1
+            if not found_lens: 
+                box.append(self.lens.lens_stuff)
+               
+        return lens_array
 
 
 
 Hashed=[instruction(d).hashfun() for d in data]
 print(sum([i.hash for i in Hashed]))
-
 # 2 # 
 
 lens_array=[[] for _ in range(256)]
 
-print([i.strength for i in Hashed])
+for instruction in Hashed:
+    instruction.process_instruction(lens_array=lens_array)
+
+print(lens_array)
+
+answer=0
+
+for box_num in range(len(lens_array)):
+    box=lens_array[box_num]
+    for lens_num in range(len(box)):
+        lens=box[lens_num]
+
+        label=list(lens.keys())[0]
+
+        answer+=(box_num+1)*(lens_num+1)*lens[label]
+
+print(answer)
